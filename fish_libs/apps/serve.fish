fee0076e04965f32ccec8819d52a98d8820b128f
tsingkwai <happyegg.chen@hotmail>
2021-08-30 18:16:07 +0800

    2021-08-30_10:16:07

--- fish_libs/apps/serve.fish
+++ fish_libs/apps/serve.fish
@@ -7,10 +7,8 @@ function __main__
     set email (sed -n '/email=/'p /etc/blueddns.conf | sed 's/email=//g')
     set authkey (sed -n '/auth-key=/'p /etc/blueddns.conf | sed 's/auth-key=//g')
     echo "curl -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsid" -H "X-Auth-Email: $email" -H "X-Auth-Key: $authkey" -H "Content-Type: application/json" --data '{"type":"A","name":"$domain","content":"$myip","proxied":false}'"
-     curl -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsid" -H "X-Auth-Email: $email" -H "X-Auth-Key: $authkey" -H "Content-Type: application/json" --data '{"type":"A","name":"'$domain'","content":"'$myip'","proxied":false}'
-     if test $status -eq 1
-     else
-       sleep 1h
+     if curl -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsid" -H "X-Auth-Email: $email" -H "X-Auth-Key: $authkey" -H "Content-Type: application/json" --data '{"type":"A","name":"'$domain'","content":"'$myip'","proxied":false}'
+     sleep 1h
      end
   end
 end
